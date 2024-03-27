eded7125e9c0d5c4bfaf86063bd28302ceac6013
Jean-Michel Friedt <friedtj@free.fr>
2024-03-27 07:24:42 +0100

    add pwm functions

---
+++ pwm/README.m
@@ -0,0 +1,4 @@
+## PWM functions
+
+* ``void pwm_configure()``: called once for initializing the timer period and pin output
+* ``void pwmD_configure()``: called for updating the duty cycle on the PWM output
--- pwm/pwm.h
+++ pwm/pwm.h
@@ -1,3 +1,2 @@
 void pwm_configure();
 void pwmD_configure();
-void pwmA_configure();
